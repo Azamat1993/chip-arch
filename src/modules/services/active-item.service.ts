55ac06e782952b96e4f6b5a75a3d25bebb44b35d
Azamat Zorkanov <zorkanov.93@gmail.com>
2021-07-16 14:40:42 +0600

    feature: active item service basic

---
+++ src/modules/services/active-item.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Activable } from "../interfaces/activeable";
+import { ClickService } from "./click.service";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ActiveItemService {
+    private readonly activeItemInterval$ = new Subject<Activable>();
+    
+    public readonly activeItem$ = this.activeItemInterval$.asObservable();
+
+    constructor(
+        private readonly clickService: ClickService,
+    ) {
+        
+    }
+
+    public setActiveItem(item: Activable) {
+        this.activeItemInterval$.next(item);
+    }
+}
