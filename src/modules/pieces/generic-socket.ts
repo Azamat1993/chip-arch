6c73b47d78d7d8dd6044fe27f93e77dd58484bcc
Azamat Zorkanov <zorkanov.93@gmail.com>
2021-08-09 00:26:11 +0600

    added basic blueprint for can move

--- src/modules/pieces/generic-socket.ts
+++ src/modules/pieces/generic-socket.ts
@@ -1,4 +1,5 @@
 import { PIECE_TYPE } from "../enums/piece-type";
+import { Point } from "../models/point";
 import { Generic } from "./generic";
 
 export abstract class GenericSocket<T> extends Generic<T> {
@@ -8,4 +9,54 @@ export abstract class GenericSocket<T> extends Generic<T> {
     protected defaultHeight = 10;
 
     protected color = 'red';
+
+    public move(diffPoint: Point): void {
+        const movedX = this.getAlignedMove(diffPoint.x);
+        const movedY = this.getAlignedMove(diffPoint.y);
+
+        if (this.hasParent()) {
+            const offsetPoint = this.position.add(diffPoint);
+            
+            if (this.isInside(offsetPoint, this.getParent())) {
+                this.moveInternal$.next(offsetPoint);
+            }
+        } else {
+            this.moveInternal$.next(diffPoint);
+        }
+    }
+
+    protected canMoveHorizontally(x: number, y: number) {
+        const parentPosition = this.parent.getPosition();
+
+        let dx = 0;
+        let dy = 0;
+        
+        if (this.position.y - this.height > parentPosition.y || this.position.y + this.parent.getHeight()) {
+
+        }
+        if (x < 0) {
+            if (this.position.x - x < this.
+        } else if (x > 0) {
+
+        }
+
+
+        if (parentPosition.y - this.height === this.position.y) {
+
+        }
+    }
+
+    protected getAlignedMove(d: number) {
+        if (d === 0) {
+            return 0;
+        }
+
+        if (d >= 1) {
+            return 1;
+        }
+
+        if (d <= 1) {
+            return -1;
+        }
+    }
 }
