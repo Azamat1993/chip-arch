f771b25e830d9eff37e6681cc2636c3a8cf66a0c
Azamat Zorkanov <zorkanov.93@gmail.com>
2021-08-10 00:09:26 +0600

    fixed move for socket

--- src/modules/pieces/generic-socket.ts
+++ src/modules/pieces/generic-socket.ts
@@ -11,39 +11,56 @@ export abstract class GenericSocket<T> extends Generic<T> {
     protected color = 'red';
 
     public move(diffPoint: Point): void {
-        const movedX = this.getAlignedMove(diffPoint.x);
-        const movedY = this.getAlignedMove(diffPoint.y);
+        const { x, y } = this.moveInRange(this.getAlignedMove(diffPoint.x), this.getAlignedMove(diffPoint.y));
+        const point = new Point(x, y);
 
         if (this.hasParent()) {
-            const offsetPoint = this.position.add(diffPoint);
+            const offsetPoint = this.position.add(point);
             
             if (this.isInside(offsetPoint, this.getParent())) {
                 this.moveInternal$.next(offsetPoint);
             }
         } else {
-            this.moveInternal$.next(diffPoint);
+            this.moveInternal$.next(point);
         }
     }
 
-    protected canMoveHorizontally(x: number, y: number) {
+    protected moveInRange(x: number, y: number) {
         const parentPosition = this.parent.getPosition();
+        const parentHeight = this.parent.getHeight();
+        const parentWidth = this.parent.getWidth();
 
         let dx = 0;
         let dy = 0;
         
-        if (this.position.y - this.height > parentPosition.y || this.position.y + this.parent.getHeight()) {
+        if (this.position.y + this.height <= parentPosition.y || this.position.y >= parentPosition.y + parentHeight) {
+            // can move horizontally
 
+            const outerX = parentPosition.x - this.width;
+            const outerWidth = parentWidth + (this.width * 2);
+
+            if (this.position.x + x >= outerX && this.position.x + x <= outerX + outerWidth) {
+                // move on x axis is in range
+                dx += x;
+            }
         }
-        if (x < 0) {
-            if (this.position.x - x < this.
-        } else if (x > 0) {
 
+        if (this.position.x + this.width <= parentPosition.x || this.position.x >= parentPosition.x + parentWidth) {
+            // can move vertically
+
+            const outerY = parentPosition.y - this.height;
+            const outerHeight = parentHeight + (this.height * 2);
+
+            if (this.position.y + y >= outerY && this.position.y + y <= outerY + outerHeight) {
+                // move on y axis is in range
+                dy += y;
+            }
         }
 
-
-        if (parentPosition.y - this.height === this.position.y) {
-
-        }
+        return {
+            x: dx,
+            y: dy,
+        };
     }
 
     protected getAlignedMove(d: number) {
